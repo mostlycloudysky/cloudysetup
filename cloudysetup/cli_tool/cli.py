93bf46accb327ab48632f340d4315cd226425f0f
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2024-06-15 07:23:46 -0400

    add `fast api` call to the cli

--- cloudysetup/cli_tool/cli.py
+++ cloudysetup/cli_tool/cli.py
@@ -1,4 +1,7 @@
 import click
+import requests
+
+BASE_URL = "http://127.0.0.1:8000"
 
 
 @click.group()
@@ -7,9 +10,14 @@ def cli():
 
 
 @cli.command()
-def hello():
-    """Prints Hello World"""
-    click.echo("Hello World")
+@click.argument("message")
+def message(message):
+    """Send a message to the FastAPI server and get a response"""
+    response = requests.post(f"{BASE_URL}/message", json={"message": message})
+    if response.status_code == 200:
+        click.echo(response.json()["message"])
+    else:
+        click.echo("Error sending message")
 
 
 if __name__ == "__main__":
--- cloudysetup/envapi_app/main.py
+++ cloudysetup/envapi_app/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class MessageRequest(BaseModel):
+    message: str
+
+
+class MessageResponse(BaseModel):
+    message: str
 
 
 @app.get("/")
 def read_root():
     return {"message:": "Hello World"}
+
+
+@app.post("/message")
+def get_message(request: MessageRequest):
+    if not request.message:
+        raise HTTPException(status_code=400, detail="Message is empty")
+
+    return {"message": request.message}
