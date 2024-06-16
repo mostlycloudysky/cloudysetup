0cbcc7063fd928f768529242fe4bacf0f3b1c83c
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2024-06-16 14:42:17 -0400

    update `monitoring status` for create resource

--- cloudysetup/cli_tool/cli.py
+++ cloudysetup/cli_tool/cli.py
@@ -1,6 +1,8 @@
 import click
 import requests
 import json
+import time
+import random
 
 BASE_URL = "http://127.0.0.1:8000"
 
@@ -23,15 +25,46 @@ def message(message, monitor):
         if monitor:
             request_token = response.json()["details"]["ProgressEvent"]["RequestToken"]
             click.echo(f"Monitoring status for request token: {request_token}")
-            # monitor_status(request_token)
+            monitor_status(request_token)
     else:
         click.echo(f"Error: {response.status_code} - {response.json().get('detail')}")
 
 
 def monitor_status(request_token):
+    """Monitor the status of the resource creation"""
     click.echo("Checking resource creation status...")
-    while True:
+    max_attempts = 10
+    wait_time = 2  # Initial wait time in seconds
+    max_wait_time = 60  # Maximum wait time in seconds
+
+    for attempt in range(max_attempts):
         click.echo(request_token)
+        response = requests.post(
+            f"{BASE_URL}/resource-status", json={"request_token": request_token}
+        )
+        if response.status_code == 200:
+            details = response.json().get("details", {})
+            status = details.get("ProgressEvent", {}).get("OperationStatus")
+            if status in ["SUCCESS", "FAILED"]:
+                click.echo(f"Operation Status: {status}")
+                formatted_details = json.dumps(details, indent=4)
+                click.echo(formatted_details)
+                break
+            else:
+                click.echo(
+                    f"Current Status: {status}. Checking again in {wait_time} seconds..."
+                )
+        else:
+            click.echo(
+                f"Error: {response.status_code} - {response.json().get('detail')}"
+            )
+
+        # Exponential backoff with jitter
+        time.sleep(wait_time)
+        wait_time = min(max_wait_time, wait_time * 2 + random.uniform(0, 1))
+
+    else:
+        click.echo("Max attempts reached. Please check the status manually.")
 
 
 def hello():
