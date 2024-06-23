4c9c95ceaef737de75468a18267e1998ffd16593
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2024-06-23 15:42:48 -0400

    add `bedrock` model access in ECS

--- iac/main.tf
+++ iac/main.tf
@@ -125,6 +125,30 @@ resource "aws_iam_role_policy_attachment" "ecs_task_role_policy_attach_cloudwatc
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
 
+resource "aws_iam_policy" "bedrock" {
+  name        = "ecs-task-policy-bedrock"
+  description = "Policy that allows access to Bedrock"
+  policy      = <<EOF
+{
+   "Version": "2012-10-17",
+   "Statement": [
+       {
+           "Effect": "Allow",
+           "Action": [
+                "bedrock:*"
+           ],
+           "Resource": "*"
+       }
+   ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_role_policy_attach_bedrock" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.bedrock.arn
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy_attach" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
